39623f593af7c9e1bd7564d42e5f1263c9c61e95
chahat <chahat.logicalstreet@gmail.com>
2021-06-08 09:41:38 +0530

    added article index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'articles/index'
   get 'user/index'
   resources :users
+  resources :articles
+  get 'article', action: :index, controller: 'articles'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_07_113403) do
+ActiveRecord::Schema.define(version: 2021_06_08_032455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "name"
+    t.string "topic"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
